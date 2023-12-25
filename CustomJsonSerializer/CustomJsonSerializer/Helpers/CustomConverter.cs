773ef0b9c81c01661947e866078ffe8e67d8f415
RasulPirsoltanov <7m0uvuf@code.edu.az>
2023-12-26 00:59:35 +0400

    first commit

---
+++ CustomJsonSerializer/CustomJsonSerializer/Helpers/CustomConverter.cs
@@ -0,0 +1,91 @@
+﻿using CustomJsonSerializer.Models;
+using Newtonsoft.Json.Linq;
+using System.Reflection;
+
+namespace CustomJsonSerializer.Extensions
+{
+    public static class CustomConverter
+    {
+        /*<summary>
+         T is generic
+        </summary>*/
+        public static T Deserialize<T>(string value) where T : class, new()
+        {
+            try
+            {
+                JObject jObject = JObject.Parse(value);
+                T rootClass = new T();
+
+                foreach (JProperty property in jObject.Properties())
+                {
+                    PropertyInfo targetProperty = rootClass.GetType().GetProperty(property.Name);
+
+                    if (targetProperty != null)
+                    {
+                        targetProperty.SetValue(rootClass, property.Value.ToObject(targetProperty.PropertyType));
+                    }
+                }
+                return rootClass;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        public static string Serializer<T>(T root) where T : class, new()
+        {
+            string newJson = "{\n";
+            foreach (var item in root.GetType().GetProperties())
+            {
+                if (!item.PropertyType.IsGenericType && item.PropertyType == typeof(string))
+                {
+                    newJson += '"' + item.Name + '"' + ':' + '"' + item.GetValue(root) + '"' + ',' + "\n";
+                }
+                else if (item.PropertyType.IsGenericType)
+                {
+                    var genericValue = item.GetValue(root);
+                    newJson += '"' + item.Name + '"' + ':' + "[";
+                    foreach (var gv in genericValue as IEnumerable<object>)
+                    {
+                        newJson += '"' + gv.ToString() + '"' + ','+"\n";
+                    }
+                    if (newJson.Last() == ',')
+                    {
+                        newJson = newJson.Remove(newJson.Length - 1);
+                    }
+                    newJson += "]" + ',' + "\n";
+                }
+                else if (item.PropertyType != typeof(string) && !item.PropertyType.IsGenericType)
+                {
+                    var genericValue = item.GetValue(root);
+                    newJson += '"' + item.Name + '"' + ':' + "{";
+                    foreach (var gv in genericValue.GetType().GetProperties())
+                    {
+                        if (gv.GetValue(genericValue).GetType()==typeof(string))
+                        {
+                            newJson += '"' + gv.Name + '"' + ":" + '"' + gv.GetValue(genericValue) + '"' + ',';
+                        }
+                        else
+                        {
+                            Console.WriteLine("test1234");
+                        }
+                    }
+                    if (newJson.Last() == ',')
+                    {
+                        newJson = newJson.Remove(newJson.Length - 1);
+                    }
+                    newJson += "}" + ',' + "\n";
+                }
+                else
+                {
+                    Console.WriteLine(item);
+                }
+
+            }
+            newJson += "}";
+            return newJson;
+        }
+    }
+}
---
+++ CustomJsonSerializer/CustomJsonSerializer/Models/RootClass.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomJsonSerializer.Models
+{
+    public class RootClass
+    {
+        public string gender { get; set; }
+        public Name name { get; set; }
+        public Location location { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string country { get; set; }
+        public string postcode { get; set; }
+        public Coordinate coordinates { get; set; }
+        public Timezone timezone { get; set; }
+        public string test { get; set; }
+    }
+    public class Name
+    {
+        public string first { get; set; }
+        public string title { get; set; }
+        public string last { get; set; }
+    }
+    public class Location
+    {
+        public Street street { get; set; }
+    }
+    public class Street
+    {
+        public string number { get; set; }
+        public string name{ get; set; }
+    }
+    public class Coordinate
+    {
+        public string latitude { get; set; }
+        public string longitude { get; set; }
+    }
+    public class Timezone
+    {
+        public string offset { get; set; }
+        public string description { get; set; }
+    }
+    public class Author
+    {
+        public string lastname { get; set; }
+        public string firstname { get; set; }
+    }
+
+    public class Editor
+    {
+        public string lastname { get; set; }
+        public string firstname { get; set; }
+    }
+
+    public class Root
+    {
+        public string isbn { get; set; }
+        public Author author { get; set; }
+        public Editor editor { get; set; }
+        public string title { get; set; }
+        public List<string> category { get; set; }
+    }
+
+}
